7d6bcfb46710e0deef741c1a08d102470af5f452
julider191199-tech <julider191199@gmail.com>
2026-02-09 20:31:33 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ozlddofurkuojjxfqus.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96bGRkb2Z1cmx4b3VqanhxZnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjQxNzcsImV4cCI6MjA1MzYwMDE3N30.7FaS5HhDQX0cGYZJJzGJBhTdOBmX2JG5ROaYcMGJ1t0'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  supabaseUrl!,
+  supabaseAnonKey!
+);
